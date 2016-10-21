c83e3339f14bbfdc4020289fddea2a57e2001402
Thomas Schiet <thomas.schiet@gmail.com>
2016-10-21 16:06:10 +0200

    Fixed dy's

--- HW2/chimera.jl
+++ HW2/chimera.jl
@@ -4,7 +4,7 @@ using DataFrames
 include("functions.jl")
 
 J = 100
-K = 50
-(x, y) = chimera_solution(J = J, K = K, h = 0.6/J, H = 0.5/K)
+K = 200
+(x, y) = chimera_solution(J = J, K = K, h = 0.5/(J-1), H = 0.5/K)
 
 plot(x = x, y = y)
