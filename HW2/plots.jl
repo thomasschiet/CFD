d4a8a603b719bc0cbeeefcea9146a06ed755b4f4
Thomas Schiet <thomas.schiet@gmail.com>
2016-10-21 12:06:30 +0200

    Fixed exact solution

--- HW2/plots.jl
+++ HW2/plots.jl
@@ -5,13 +5,13 @@ include("functions.jl")
 
 a=0.1
 b=1
-pe=40
+pe = 40
 scheme="central1"
 refinement = true
-m1 = 200
-m2 = 500
+m1 = 20
+m2 = 40
 
-q(x) = -2/π * a * sinpi(x/2) + 4/(π^2 * pe) * a * cospi(x/2) + (1/pe - 1)*b/(1-e)*exp(x)
+q(x) = -π/2 * a * sinpi(x/2) + π^2/(4 * pe) * a * cospi(x/2) + (1/pe - 1)*b/(1-e)*exp(x)
 
 (ϕ_n, x) = numerical_solution(a=a, b=b, pe=pe, scheme=scheme, refinement=refinement, m1=m1, m2=m2, q = q)
 df_n = DataFrame(x=x, y=ϕ_n, label="Numerical")
@@ -20,7 +20,7 @@ df_n = DataFrame(x=x, y=ϕ_n, label="Numerical")
 ϕ_e = map(x_1 -> a * cospi(x_1/2) + b / (1-e) * (1 - exp(x_1)), x)
 df_e = DataFrame(x = x, y = ϕ_e, label="Exact")
 
-df = vcat(df_n)
+df = vcat(df_n, df_e)
 
 p = plot(df, x="x", y="y", color="label", Geom.line,
          Scale.discrete_color_manual("blue","red"))
