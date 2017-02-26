a9ec1614ffb04cfad1ab26f8c2a8bc7526c7680d
Thomas Schiet <thomas.schiet@gmail.com>
2017-02-26 13:26:03 +0100

    Build matrix

--- 2_2/ex5.m
+++ 2_2/ex5.m
@@ -6,3 +6,23 @@ R_1 = 1;
 R_2 = 5;
 
 %% 
+A = sparse(n_r, n_theta);
+
+d_r = (R_2-R_1) / n_r;
+d_theta = pi / n_theta;
+
+for i = 2:n_r-1
+    for j = 2:n_theta-1
+        r_i = R_1 + d_r * i;
+        a = d_r^2 / (2*d_r^2 + r_i*d_r*d_theta^2 + 2*r_i^2 * d_theta^2);
+        b = d_r^2 / (2*d_r^2 + r_i*d_r*d_theta^2 + 2*r_i^2 * d_theta^2);
+        c = (r_i*d_r*d_theta^2 + r_i^2*d_theta^2) / (2*d_r^2 + r_i*d_r*d_theta^2 + 2*r_i^2 * d_theta^2);
+        d = r_i^2*d_theta^2 / (2*d_r^2 + r_i*d_r*d_theta^2 + 2*r_i^2 * d_theta^2);
+        
+        row = (i-1)*n_r + j;
+        A(row, row + 1) = a;
+        A(row, row - 1) = b;
+        A(row, row + n_r) = c;
+        A(row, row - n_r) = d;
+    end
+end
