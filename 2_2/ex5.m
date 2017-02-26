99e140a661076bed7c131c735569807007127b96
Thomas Schiet <thomas.schiet@gmail.com>
2017-02-26 17:07:32 +0100

    Worked a bit on the matrix

--- 2_2/ex5.m
+++ 2_2/ex5.m
@@ -4,6 +4,8 @@ n_theta = 10;
 U_infty = 1;
 R_1 = 1;
 R_2 = 5;
+Delta_eta_1 = 1/(n_theta-1);
+Delta_eta_2 = 1/(n_r-1);
 
 %% 
 A = sparse(n_r, n_theta);
@@ -11,18 +13,29 @@ A = sparse(n_r, n_theta);
 d_r = (R_2-R_1) / n_r;
 d_theta = pi / n_theta;
 
-for i = 2:n_r-1
-    for j = 2:n_theta-1
-        r_i = R_1 + d_r * i;
-        a = d_r^2 / (2*d_r^2 + r_i*d_r*d_theta^2 + 2*r_i^2 * d_theta^2);
-        b = d_r^2 / (2*d_r^2 + r_i*d_r*d_theta^2 + 2*r_i^2 * d_theta^2);
-        c = (r_i*d_r*d_theta^2 + r_i^2*d_theta^2) / (2*d_r^2 + r_i*d_r*d_theta^2 + 2*r_i^2 * d_theta^2);
-        d = r_i^2*d_theta^2 / (2*d_r^2 + r_i*d_r*d_theta^2 + 2*r_i^2 * d_theta^2);
+ind = @(i, j) (i-1)*n_r + j;
+
+for i = 1:n_r
+    for j = 1:n_theta
+        row = ind(i, j);
+        if i ~= 1 && i ~= n_r && j ~= 1 && j ~= n_theta
+            eta_1 = (j - 1) / (n_theta - 1);
+            eta_2 = (i - 1) / (n_r - 1);
+            lambda = (2*Delta_eta_2/eta_2/Delta_eta_1 + (eta_2^2+(eta_2 + 1/(n_theta-1))^2)/Delta_eta_2)^-1;
+            a = lambda * eta_2 * Delta_eta_2 / Delta_eta_1;
+            b = -b;
+            c = lambda * (eta_2^2 + (eta_2 + 1/(n_theta-1))^2);
+            d = -c;
+
+            A(row, row + 1) = a;
+            A(row, row - 1) = b;
+            A(row, row + n_r) = c;
+            A(row, row - n_r) = d;
+        end
         
-        row = (i-1)*n_r + j;
-        A(row, row + 1) = a;
-        A(row, row - 1) = b;
-        A(row, row + n_r) = c;
-        A(row, row - n_r) = d;
+        if i > 1 && i < n_r && j ~= 1 && j ~= n_theta
+            lambda = 
+        end
     end
 end
+
