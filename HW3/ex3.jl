b2cf55ffb83591c991e337c5d65c5a4c19a7c475
LarsZondag <larszondag@gmail.com>
2017-07-02 15:31:22 +0200

    plot several lhats

--- HW3/ex3.jl
+++ HW3/ex3.jl
@@ -1,7 +1,7 @@
 using Gadfly
 using Cairo
 using Contour
-using Fontconfig
+# using Fontconfig
 
 u = 1
 h = 1
@@ -22,13 +22,11 @@ Dhat(θ, Pe) = δ(θ, Pe)
 Lhat(θ, κ, Pe) = Chat(θ, κ) + Dhat(θ, Pe)
 
 layers = Any[]
-for κ in 0:0.1:1
-  xs = linspace(-4, 1, 1500)
-  ys = linspace(-4, 4, 1500)
-  zs = [abs(Lhat(x + im*y, κ, 100000)) for x in xs, y in ys]
-  cont = contour(xs, ys, zs, 1)
-  plot_x, plot_y = coordinates(lines(cont)[1])
-  push!(layers, layer(x = plot_x, y = plot_y, Geom.point))
+for Pe in logspace(0.1,1,5)
+  θs = linspace(0, 2, 1500)
+  LhatValues = [-Lhat(x, 1/3, Pe) for x in θs]
+
+  push!(layers, layer(x = real(LhatValues), y = imag(LhatValues), Geom.point))
 end
 
 R(λτ) = 1 + λτ + 1/2*(λτ)^2 + 1/6 * (λτ)^3 + 1/24 * (λτ)^4 + 1/144 * (λτ)^5
@@ -40,7 +38,7 @@ cont = contour(xs, ys, zs, 1)
 plot_x, plot_y = coordinates(lines(cont)[1])
 spaceship = layer(x = plot_x, y = plot_y, Geom.point)
 
-plot(layer_0)
+# plot(layer_0)
 plot(layers..., spaceship,
   Theme(
     panel_fill = colorant"white",
