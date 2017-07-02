3db8cd06f3518fd88cb5cb3f9107429902e85a0a
LarsZondag <larszondag@gmail.com>
2017-07-02 15:41:42 +0200

    new plot for kappa dep

--- HW3/ex3.jl
+++ HW3/ex3.jl
@@ -21,12 +21,21 @@ Chat(θ, κ) = γ_1(θ, κ) + im * γ_2(θ, κ)
 Dhat(θ, Pe) = δ(θ, Pe)
 Lhat(θ, κ, Pe) = Chat(θ, κ) + Dhat(θ, Pe)
 
-layers = Any[]
+# Make the datasets for the Pe dependence
+layersForPe = Any[]
 for Pe in logspace(0.1,1,5)
   θs = linspace(0, 2, 1500)
   LhatValues = [-Lhat(x, 1/3, Pe) for x in θs]
 
-  push!(layers, layer(x = real(LhatValues), y = imag(LhatValues), Geom.point))
+  push!(layersForPe, layer(x = real(LhatValues), y = imag(LhatValues), Geom.point))
+end
+
+layersForκ = Any[]
+for κ in linspace(0,1,5)
+  θs = linspace(0, 2, 1500)
+  LhatValues = [-Lhat(x, κ, 10000) for x in θs]
+
+  push!(layersForκ, layer(x = real(LhatValues), y = imag(LhatValues), Geom.point))
 end
 
 R(λτ) = 1 + λτ + 1/2*(λτ)^2 + 1/6 * (λτ)^3 + 1/24 * (λτ)^4 + 1/144 * (λτ)^5
@@ -38,11 +47,20 @@ cont = contour(xs, ys, zs, 1)
 plot_x, plot_y = coordinates(lines(cont)[1])
 spaceship = layer(x = plot_x, y = plot_y, Geom.point)
 
-# plot(layer_0)
-plot(layers..., spaceship,
+# plot for Pe dependence
+plot(layersForPe..., spaceship,
   Theme(
     panel_fill = colorant"white",
     default_point_size = 0.5pt,
     default_color = colorant"black",
     highlight_width = 0pt
   ))
+
+  # plot for κ dependence
+# plot(layersForκ..., spaceship,
+#   Theme(
+#     panel_fill = colorant"white",
+#     default_point_size = 0.5pt,
+#     default_color = colorant"black",
+#     highlight_width = 0pt
+#   ))
